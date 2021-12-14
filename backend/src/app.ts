177c147778ca6405ba526834319f6809e5641314
José Cortés <cote99salamanca@gmail.com>
2021-12-14 12:11:58 -0300

    config server and add middlewares & routes

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+//initialize express
+const app = express();
+
+//settings
+app.set("port", process.env.PORT || 4000);
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+//routes
+
+app.get("/", (req: Request, res: Response): Response => {
+  return res.send(`The API is at http://localhost:${app.get("port")}`);
+});
+
+export default app;
