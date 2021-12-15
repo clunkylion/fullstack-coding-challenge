bd2fef13e435dd5a39283fffb5382571ea4a8617
José Cortés <cote99salamanca@gmail.com>
2021-12-15 13:38:41 -0300

    import interfaces and change name in methods

--- frontend/src/Hooks/Auth/useAuthProvider.tsx
+++ frontend/src/Hooks/Auth/useAuthProvider.tsx
@@ -1,94 +1,57 @@
-import React, { useCallback, useState, ReactChildren, ReactChild } from "react";
-
-interface IAuthContext {
-  loginState: {
-    email: string;
-    password: string;
-  };
-  signUpState: {
-    name: string;
-    lastname: string;
-    email: string;
-    password: string;
-    gender: number;
-  };
-  updateLoginState: (email: string, password: string) => void;
-  updateSignUpState: (
-    name: string,
-    lastname: string,
-    email: string,
-    password: string,
-    gender: number
-  ) => void;
-}
+import React, { useState } from "react";
+import {IAuthProviderProps,ILoginState,ISignUpState,IUserState, IAuthContext} from './interfaces'
 
 const initialState: IAuthContext = {
   loginState: { email: "", password: "" },
-  updateLoginState: () => {},
+  setLoginState: () => {},
   signUpState: {
     name: "",
     lastname: "",
     email: "",
     password: "",
-    gender: 1,
+    gender: '',
   },
-  updateSignUpState: () => {},
+  setSignUpState: () => {},
+  user: {
+    _id: '',
+    name: '',
+    gender: '',
+    email: '',
+    lastName: '', 
+    password: ''
+  },
+  setUser: () => {}
 };
 export const AuthContext = React.createContext(initialState);
 
-interface IAuthProviderProps {
-  children: ReactChildren | ReactChild;
-}
+
 const AuthProvider = ({ children }: IAuthProviderProps) => {
-  const [loginState, setLoginState] = useState({
+  const [loginState, setLoginState] = useState<ILoginState>({
     email: "",
     password: "",
   });
-  const [signUpState, setSignUpState] = useState({
+  const [signUpState, setSignUpState] = useState<ISignUpState>({
     name: "",
     lastname: "",
     email: "",
     password: "",
-    gender: 1,
+    gender: '',
   });
-  const updateLoginState = (email: string, password: string) =>
-    useCallback(
-      (email: string, password: string) =>
-        setLoginState({
-          email,
-          password,
-        }),
-      []
-    );
-  const updateSignUpState = (
-    name: string,
-    lastname: string,
-    email: string,
-    password: string,
-    gender: number
-  ) =>
-    useCallback(
-      (
-        name: string,
-        lastname: string,
-        email: string,
-        password: string,
-        gender: number
-      ) =>
-        setSignUpState({
-          name,
-          password,
-          lastname,
-          gender,
-          email,
-        }),
-      []
-    );
+  const [user, setUser] = useState<IUserState>({
+    _id: '',
+    name: '',
+    gender: '',
+    email: '',
+    lastName: '', 
+    password: ''
+  })
   const contextValue: IAuthContext = {
     loginState,
-    updateLoginState,
+    setLoginState,
     signUpState,
-    updateSignUpState,
+    setSignUpState,
+    user,
+    setUser
   };
   return (
     <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
