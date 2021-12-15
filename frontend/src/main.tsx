187803fb3b8a45250ec2862e9129d73845f3ca0e
José Cortés <cote99salamanca@gmail.com>
2021-12-15 13:36:19 -0300

    add toaster and provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import 'bootswatch/dist/lux/bootstrap.min.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootswatch/dist/lux/bootstrap.min.css";
+import App from "./App";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./Hooks/Auth/useAuthProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Toaster />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
