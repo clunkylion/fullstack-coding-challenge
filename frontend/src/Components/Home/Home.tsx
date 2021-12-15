b68c30924093820a87adfb8ec8f3cf8e678b0ce5
José Cortés <cote99salamanca@gmail.com>
2021-12-15 13:37:08 -0300

    homeComponent

--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -1,7 +1,30 @@
+import useAuth from "../../Hooks/Auth";
+import { useHistory } from "react-router-dom";
 export const Home = () => {
-    return (
-        <div className="App">
-            Home
+  const history = useHistory();
+  const { user, setLoginState } = useAuth();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setLoginState({
+        email: "",
+        password: "",
+    });
+    history.push("/");
+  }
+  return (
+    <div className="App">
+      <div className="container mt-5">
+        <div className="row d-flex justify-content-center mt-5">
+          <div className="col-md-6">
+            <div className="card px-5 py-5">
+              <div className="my-3">
+                <h3>Hello:  {user.name} {user.lastName}</h3>
+              </div>
+              <button className="btn btn-dark w-100" onClick={handleLogout}>Logout</button>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
